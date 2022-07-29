d2d20c9dfe4f6adddc153294be0c71cdae97935b
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-07-29 22:51:53 +0800

    fix spinner position

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { GridLoader } from "react-spinners";
 
 const Spinner = () => {
 	return (
-		<div className="absolute left-1/2 top-1/2">
+		<div className="absolute left-1/2 top-1/2 -translate-x-1/2">
 			<GridLoader className="spinner" color="#36D7B7" />
 		</div>
 	);
